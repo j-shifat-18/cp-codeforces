f70b7b8cdb1babbcf8274356ba9d63f6257b76c4
j-shifat-18 <info.jahirulsifat@gmail.com>
2024-08-19 20:07:51 +0600

    1st file

---
+++ watermelon.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int weight ;
+    cin>>weight;
+
+    if( weight>2 && weight%2 == 0 )
+    {
+        cout<<"YES";
+    }
+    else cout<<"NO";
+
+
+    return 0;
+}
