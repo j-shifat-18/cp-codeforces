555a59c4832f4b6e0847b366b1babc1befcac833
j-shifat-18 <info.jahirulsifat@gmail.com>
2024-08-26 14:07:01 +0600

    QAQ file

---
+++ QAQ.cpp
@@ -0,0 +1,40 @@
+// https://codeforces.com/problemset/problem/894/A
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string word;
+    cin >> word;
+
+    int size = word.length();
+   //int size=100;
+    int count = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (word[i] == 'Q')
+        {
+            for (int j = i + 1; j < size  ; j++)
+            {
+                if (word[j] == 'A')
+                {
+                    for (int k = j + 1; k < size ; k++)
+                    {
+                        if (word[k] == 'Q')
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
